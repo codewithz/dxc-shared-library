ff3196aeccde2966d216742b98a515d3abef5098
Code With Z <71726246+codewithz@users.noreply.github.com>
2021-11-26 15:27:03 +0530

    Add files via upload

---
+++ vars/sayHelloOrHi.groovy
@@ -0,0 +1,23 @@
+def call(String sayWhat="nothing"){
+    if(sayWhat=="hi"){
+        script{
+            bat """
+                echo Hi Zartab
+            """
+        }
+    } 
+    else if(sayWhat=="hello"){
+         script{
+            bat """
+                echo Hello Zartab
+            """
+         }
+    }
+    else{
+         script{
+            bat """
+                echo Whats Up Zartab
+            """
+         }
+    }
+}
